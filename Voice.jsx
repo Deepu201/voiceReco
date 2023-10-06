124edd2265e7ffdae2224de082b7dac2a0252a6f
Deepu201 <deepu7652k@gmail.com>
2023-10-06 20:02:55 +0530

    v

---
+++ Voice.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+const Voice = () => {
+
+    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition({ commands })
+
+  if (!browserSupportsSpeechRecognition) {
+    return null
+  }
+
+  return (
+    <>
+    <div>
+        {transcript}
+    </div>
+    <button onClick={SpeechRecognition.startListening}>start</button>
+    <button onClick={SpeechRecognition.stopListening}>stop</button>
+    </>
+  )
+}
+
+export default Voice
