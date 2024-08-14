316fe2223428fa8c9eaebdfdfec5c4b59953b24b
Deepu201 <deepu7652k@gmail.com>
2024-08-14 19:31:42 +0530

    f

--- Voice.jsx
+++ Voice.jsx
@@ -11,6 +11,7 @@ const voice=()=>SpeechRecognition.startListening({continuous:true,language:"en-I
   return (
     <>
     <div>:_____________________________________________________:
+      <div>hiiiiii</div>
         {transcript}
     </div>
     <button onClick={voice}>start</button>
